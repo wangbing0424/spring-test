70e233a871867ecbf7f727611d284790fc652e90
wangbing0424 <wangbing0424@163.com>
2015-06-15 14:24:25 +0800

    test5

--- src/main/java/com/ftpserver/test/test_test/Test.java
+++ src/main/java/com/ftpserver/test/test_test/Test.java
@@ -3,7 +3,7 @@ package com.ftpserver.test.test_test;
 public class Test {
 
 	public static void main(String[] args) {
-       System.out.println("hello java test 4");
+       System.out.println("hello java test 5");
 	}
 
 }
